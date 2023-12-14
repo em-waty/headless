5eec1f0f139d9c2e83cbedae6f7a8bcd7ddbcd28
em-waty <153804088+em-waty@users.noreply.github.com>
2023-12-14 09:19:18 +0100

    Create page-transition-contact.js

---
+++ page-transition-contact.js
@@ -0,0 +1,59 @@
+let transitionTrigger = $(".page-loader_trigger");
+let introDurationMS = 1600;
+let exitDurationMS = 1200;
+let excludedClass = "no-transition";
+let animationsInitialized = false; // Flag to check if GSAP animations have been initialized
+
+// On Page Load
+if (transitionTrigger.length > 0) {
+  transitionTrigger.click();
+  $("body").addClass("no-scroll-transition");
+
+  // 80% of introDurationMS
+  let animationStartDelay = 0.5 * introDurationMS;
+
+  setTimeout(() => {
+    // Emit an event when 80% of the loader duration has passed.
+    $(document).trigger("loaderEightyPercentComplete");
+  }, animationStartDelay);
+
+  setTimeout(() => {
+    $("body").removeClass("no-scroll-transition");
+    reinitializeGSAPAnimations(); // Setup GSAP animations after intro
+  }, introDurationMS);
+}
+
+// On Link Click
+$(document).on("click", "a", function (e) {
+  if (
+    $(this).prop("hostname") == window.location.host &&
+    $(this).attr("href").indexOf("#") === -1 &&
+    !$(this).hasClass(excludedClass) &&
+    $(this).attr("target") !== "_blank" &&
+    transitionTrigger.length > 0
+  ) {
+    e.preventDefault();
+    $("body").addClass("no-scroll-transition");
+    let transitionURL = $(this).attr("href");
+    transitionTrigger.click();
+    setTimeout(function () {
+      window.location = transitionURL;
+    }, exitDurationMS);
+  }
+});
+
+// On Back Button Tap
+window.onpageshow = function (event) {
+  if (event.persisted) {
+    window.location.reload();
+  }
+};
+
+// Hide Transition on Window Width Resize
+setTimeout(() => {
+  $(window).on("resize", function () {
+    setTimeout(() => {
+      $(".page-loader").css("display", "none");
+    }, 50);
+  });
+}, introDurationMS);
